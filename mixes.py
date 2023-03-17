950e8d32ddcfed89a709ad5e884efa22edcca31f
Those <mixmix23@gmail.com>
2023-03-17 12:17:19 -0500

    from Eric

--- mixes.py
+++ mixes.py
@@ -5,25 +5,6 @@ import streamlit as st
 tree = ET.parse('01-Mixes-001.xml')
 root = tree.getroot()
 
-# Find the "Mix" elements in the XML file
-mixes = root.findall(".//MixHeader")
-
-# Iterate over the "Mix" elements
-for mix in mixes:
-    # Extract the relevant information from the "Mix" element
-    name = mix.get("MixFormula")
-    constituents = mix.findall(".//Constituent")
-    constituents_table = []
-
-    # Iterate over the "Constituent" elements and build a table
-    for constituent in constituents:
-        constituents_table.append({
-            "Name": constituent.get("ConstituentCode"),
-            "Dosage": constituent.get("Dosage")
-        })
-
-    # Display the mix information and the constituents table using an expander
-    with st.beta_expander(f"{name}"):
-        st.write(f"Mix name: {name}")
-        st.table(constituents_table)
+for element in root.iter():
+    st.write(f"{element.tag} {element.attrib}")
 
