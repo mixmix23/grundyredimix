81945ea90af6c1b2ec1993cac49922d476ab30ff
Those <mixmix23@gmail.com>
2023-03-17 12:08:43 -0500

    from Eric

--- mixes.py
+++ mixes.py
@@ -1,21 +1,31 @@
-import streamlit as st
-import pandas as pd
 import xml.etree.ElementTree as ET
+import streamlit as st
 
 # Parse the XML file
 tree = ET.parse('01-Mixes-001.xml')
 root = tree.getroot()
 
-# Extract the necessary information to build the table
-table_data = []
-for child in root:
-    row = {}
-    for subchild in child:
-        row[subchild.tag] = subchild.text
-    table_data.append(row)
+# Find the "Mix" elements in the XML file
+mixes = root.findall(".//Mix")
 
-# Create a dataframe from the extracted information
-df = pd.DataFrame(table_data)
+# Iterate over the "Mix" elements
+for mix in mixes:
+    # Extract the relevant information from the "Mix" element
+    name = mix.get("Name")
+    date = mix.get("Date")
+    constituents = mix.findall(".//Constituent")
+    constituents_table = []
 
-# Display the table using Streamlit
-st.table(df)
+    # Iterate over the "Constituent" elements and build a table
+    for constituent in constituents:
+        constituents_table.append({
+            "Name": constituent.get("Name"),
+            "Percentage": constituent.get("Percentage")
+        })
+
+    # Display the mix information and the constituents table using an expander
+    with st.beta_expander(f"{name} ({date})"):
+        st.write(f"Mix name: {name}")
+        st.write(f"Mix date: {date}")
+        st.table(constituents_table)
+
