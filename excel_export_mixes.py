525d3bd1f7e67a08fcd67eee37ca503a3c575312
Those <mixmix23@gmail.com>
2023-03-17 13:27:34 -0500

    from Eric

--- excel_export_mixes.py
+++ excel_export_mixes.py
@@ -45,6 +45,6 @@ st.dataframe(df)
 # Button to export the data to an Excel file
 if st.button("Export to Excel"):
     file_name = f"mixes_{plant_code}.xlsx"
-    file_path = f'/Users/mixmix/Desktop/{file_name}'
+    file_path = f'/Users/mixmi/Desktop/{file_name}'
     df.to_csv(file_path, index=False)
     st.success(f"Data exported to: {file_name}!")
