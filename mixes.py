3299dd414e93586992d3f8ebb9400c27de756dca
Those <mixmix23@gmail.com>
2023-03-17 12:14:31 -0500

    from Eric

--- mixes.py
+++ mixes.py
@@ -6,26 +6,24 @@ tree = ET.parse('01-Mixes-001.xml')
 root = tree.getroot()
 
 # Find the "Mix" elements in the XML file
-mixes = root.findall(".//Mix")
+mixes = root.findall(".//MixHeader")
 
 # Iterate over the "Mix" elements
 for mix in mixes:
     # Extract the relevant information from the "Mix" element
-    name = mix.get("Name")
-    date = mix.get("Date")
+    name = mix.get("MixFormula")
     constituents = mix.findall(".//Constituent")
     constituents_table = []
 
     # Iterate over the "Constituent" elements and build a table
     for constituent in constituents:
         constituents_table.append({
-            "Name": constituent.get("Name"),
-            "Percentage": constituent.get("Percentage")
+            "Name": constituent.get("ConstituentCode"),
+            "Dosage": constituent.get("Dosage")
         })
 
     # Display the mix information and the constituents table using an expander
-    with st.beta_expander(f"{name} ({date})"):
+    with st.beta_expander(f"{name}"):
         st.write(f"Mix name: {name}")
-        st.write(f"Mix date: {date}")
         st.table(constituents_table)
 
