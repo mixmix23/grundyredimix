9d510c9d9065b635d1c1b7fb4033e2de568c0323
Those <mixmix23@gmail.com>
2023-03-17 12:37:57 -0500

    from Eric

--- mixes.py
+++ mixes.py
@@ -11,12 +11,14 @@ mix_headers = root.findall(".//MixHeader")
 for mix_header in mix_headers:
     mix_number = mix_header.find("MixNumber").text
     mix_description = mix_header.find("MixDescription").text
+    plant = mix_header.find("PlantCode").text
     constituents = mix_header.findall(".//Constituents")
 
     # Display the mix number and the constituents
     with st.beta_expander(f"Mix {mix_number}"):
         st.write(f"Mix number: {mix_number}")
         st.write(f"Mix Description: {mix_description}")
+        st.write(f"Plant: {plant}")
         for constituent in constituents:
             constituent_code = constituent.find("ConstituentCode").text
             dosage = constituent.find("Dosage").text
