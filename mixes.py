fc7cd573689f5d262530d0ac53afdf05d79bd3b6
Those <mixmix23@gmail.com>
2023-03-17 12:33:15 -0500

    from Eric

--- mixes.py
+++ mixes.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 import streamlit as st
 
 # Parse the XML file
-tree = ET.parse('sample.xml')
+tree = ET.parse('01-Mixes-001.xml')
 root = tree.getroot()
 
 # Find the "MixHeader" elements in the XML file
