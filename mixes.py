c5dbc57b6fca5d883c7b1257c20a5a1b50f9fc79
Those <mixmix23@gmail.com>
2023-03-17 12:24:20 -0500

    from Eric

--- mixes.py
+++ mixes.py
@@ -6,7 +6,7 @@ tree = ET.parse('01-Mixes-001.xml')
 root = tree.getroot()
 
 # Find the "MixNumber" elements in the XML file
-mix_numbers = root.findall(".//MixNumber")
+mix_numbers = root.findall(".//MixHeader")
 
 # Iterate over the "MixNumber" elements and print their values
 for mix_number in mix_numbers:
