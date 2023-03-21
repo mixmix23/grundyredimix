52381b28614b991d6bc0e5f58499c020b1b7b174
Those <mixmix23@gmail.com>
2023-03-21 14:04:11 -0500

    from Eric

--- driver_schedule_report.py
+++ driver_schedule_report.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pytz
 import requests
 import streamlit as st
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import os
