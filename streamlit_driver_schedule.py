39ca71f44f4f15f66810bb061ac9585a4d51697f
Those <mixmix23@gmail.com>
2023-03-21 14:29:09 -0500

    from Eric

--- streamlit_driver_schedule.py
+++ streamlit_driver_schedule.py
@@ -2,7 +2,54 @@ import dateutil.parser
 import pandas as pd
 import pytz
 import streamlit as st
-from driver_schedule_report import get_employee_data, get_schedule_data
+
+api_key = "9A2B3075-33A5-42FD-9831-3A6ACEAE97F4"
+headers = {'X-API-KEY': f'{api_key}'}
+
+
+def get_employee_data():
+    url = 'https://dfapi.digitalfleet.com/api/v2/Users?page=1&pageSize=100'
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        employee_data = []
+        data = response.json()
+        print('Employee Data Full Keys')
+        print(list(data["data"][0].keys()))
+        for item in data['data']:
+            employee_data.append(
+                {'userId': item['userId'], 'firstName': item['firstName'], 'lastName': item['lastName'],
+                 'pin': item['pin'], 'hireDate': item['hireDate'], 'cellNumber': item['cellNumber']})
+        print('Employee Data Filtered Keys')
+        print(list(employee_data[0].keys()))
+        # for item in employee_data:
+        #     print(item)
+        return employee_data
+    else:
+        print(f"Error: Failed to retrieve data from {url}")
+
+
+def get_schedule_data():
+    url = 'https://dfapi.digitalfleet.com/api/v2/Schedule?page=1&pageSize=100'
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        schedule_data = []
+        data = response.json()
+        print('Schedule Data Full Keys')
+        print(list(data["data"][0].keys()))
+        for item in data['data']:
+            schedule_data.append(
+                {'userId': item['userId'], 'plantPointId': item['plantPointId'], 'scheduleDate': item['scheduleDate'],
+                 'seniority': item['seniority'], 'notes': item['notes'], 'startTime':
+                     item['startTime'], 'availability': item['availability']})
+        print('Schedule Data Filtered Keys')
+        print(list(schedule_data[0].keys()))
+        # for item in schedule_data:
+        #     print(item)
+        return schedule_data
+    else:
+        print(f"Error: Failed to retrieve data from {url}")
 
 employee_list = get_employee_data()
 schedule_list = get_schedule_data()
