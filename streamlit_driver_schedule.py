6189075d6e696f6ff275e5880faeed5863146003
Eric <mixmix23@gmail.com>
2023-03-21 19:39:11 -0500

    from Eric

--- streamlit_driver_schedule.py
+++ streamlit_driver_schedule.py
@@ -33,7 +33,7 @@ def get_employee_data():
 
 def get_schedule_data(iso_date_arg):
     # iso_date_arg = '2023-03-07T00'
-    url = f'https://dfapi.digitalfleet.com/api/v2/Schedule?endTime={iso_date_arg}%3A00%3A00&page=1&pageSize=100'
+    url = f'https://dfapi.digitalfleet.com/api/v2/Schedule?startTime={iso_date_arg}%3A00%3A00&page=1&pageSize=100'
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
