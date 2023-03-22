6964dbb61dc1658b6bfcf6cd849ad8b78cd8cd5f
Those <mixmix23@gmail.com>
2023-03-22 08:55:29 -0500

    from Eric

--- excel_export_mixes.py
+++ excel_export_mixes.py
@@ -4,26 +4,26 @@ import pandas as pd
 import os
 
 st.set_page_config(page_title="Mixes XML to CSV")
-
 # Sidebar to select plant code
 plant_code = st.sidebar.radio("Select plant code", ["001", "002", "003", "004", "005"])
+
+# Filter by mix name
+col1, col2 = st.columns([3, 1])
+mix_name_filter = col1.text_input('Search Mix', placeholder='mix')
+st.write(mix_name_filter)
 
 # Parse the XML file
 tree = ET.parse(f"mixes_xml/01-Mixes-{plant_code}.xml")
 root = tree.getroot()
-
 # Find the "MixHeader" elements in the XML file
 mix_headers = root.findall(".//MixHeader")
-
 # Create a list to store the data
 mix_list = []
-
 for mix_header in mix_headers:
     mix_number = mix_header.find("MixNumber").text
     mix_description = mix_header.find("MixDescription").text
     plant = mix_header.find("PlantCode").text
     constituents = mix_header.findall(".//Constituents")
-
     # Create a dictionary to store the data for each mix
     mix_data = {
         "Mix Number": mix_number,
@@ -34,14 +34,8 @@ for mix_header in mix_headers:
         constituent_code = constituent.find("ConstituentCode").text
         dosage = constituent.find("Dosage").text
         mix_data[constituent_code] = dosage
-
     # Append the mix data to the list
     mix_list.append(mix_data)
-
-# Filter by mix name
-col1, col2 = st.columns([3, 1])
-mix_name_filter = col1.text_input('Search Mix', placeholder='mix')
-
 
 # Create a DataFrame from the data
 df = pd.DataFrame(mix_list)
@@ -51,15 +45,12 @@ st.dataframe(df)
 
 # Specify the directory to save the CSV file
 downloads_dir = os.path.join(os.path.expanduser("~"), "Downloads")
-
 if not os.path.exists(downloads_dir):
     os.makedirs(downloads_dir)
-
 # Save the DataFrame to a CSV file
 filename = f"01-Mixes-{plant_code}.csv"
 filepath = os.path.join(downloads_dir, filename)
 df.to_csv(filepath, index=False)
-
 # Create a download button for the CSV file
 with open(filepath, "rb") as f:
     st.download_button(
