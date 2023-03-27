5c2877eb5403c2a6bcc28b89af38e666f8c442ef
Those <mixmix23@gmail.com>
2023-03-27 15:35:13 -0500

    from Eric

--- streamlit_driver_schedule.py
+++ streamlit_driver_schedule.py
@@ -151,12 +151,14 @@ for item in schedule_report:
     iso_start_time = dateutil.parser.parse(iso_date_str)
     # print('iso_start_time %s' % iso_start_time)
     localtime = iso_start_time.astimezone(pytz.timezone("US/Central"))
-    # print('localtime %s' % localtime)
+    print('localtime %s' % localtime)
+    format_local_time = localtime.strftime("%a %b %d %H:%M")
+    print(format_local_time)
     start_time = localtime.ctime()
     csv_file_format = localtime.strftime('%a %b %d %Y')
     # print('start time %s' % start_time)
-    start_time_without_year = ' '.join(start_time.split()[:4])
-    start_time_without_seconds = start_time_without_year[:-3]
+    # start_time_without_year = ' '.join(start_time.split()[:4])
+    # start_time_without_seconds = start_time_without_year[:-3]
 
     name = f"{item['firstName']} {item['lastName']}"
     if isinstance(item, dict):
@@ -164,7 +166,7 @@ for item in schedule_report:
             item['hireDate'],
             name,
             plantId,
-            start_time_without_seconds,
+            format_local_time,
             dead_head
         ])
     df = pd.DataFrame(data,
