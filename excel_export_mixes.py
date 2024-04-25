960278bbdab342b0562f3e1fa8e2a6ed13aba74d
Those <mixmix23@gmail.com>
2024-04-25 12:45:11 -0500

    from Eric

--- excel_export_mixes.py
+++ excel_export_mixes.py
@@ -168,9 +168,9 @@ def create_dataframe_csv():
                                    'Total Cost'])
         df['Total Cost'] = df['Total Cost'].map("{:.2f}".format)
 
-        # # Create a filter button
-        # if st.button("Filter Total Cost"):
-        #     st.write(df['Total Cost'])
+    # Create a filter button
+    if st.button("Filter Total Cost"):
+        st.write(df['Total Cost'])
 
     # Display the data in a table
     if len(df_list) > 0:
